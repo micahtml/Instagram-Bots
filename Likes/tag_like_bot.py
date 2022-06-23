65e61603d53f4b933dadfea909bad354a029639e
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2022-06-22 23:39:06 -0500

    instructions included

--- Likes/tag_like_bot.py
+++ Likes/tag_like_bot.py
@@ -3,10 +3,10 @@ from instapy import InstaPy
 #login credentials
 insta_username = ''
 insta_password = ''
-
 #login session
 session = InstaPy(username=insta_username, password=insta_password)
 session.login()
 
-#Pass desired tags in strings in the first argument and the Like limit in second argument
-session.like_by_tags(["cars", "Ferari"], amount=10)
+# Like posts based on hashtags and like 3 posts of its poster
+session.set_user_interact(amount=3, randomize=True, percentage=100, media='Photo')
+session.like_by_tags(['natgeo', 'world'], amount=10, interact=True)
